660267e1a523fd11090c3be1f6d2a9470b6b1af5
wael ali <waelali595@gmail.com>
2018-02-01 11:57:44 +0100

    Question object + showing the question in the console

--- console_game.js
+++ console_game.js
@@ -2,13 +2,41 @@
 var Question  = function(question, rightAnswer){
   this.question = question;
   this.rightAnswer  = rightAnswer;
-  this.options = function(params){
-    for (var i = 0; i < params.length; i++) {
-     console.log(i + '- ' + params[i] ); 
-    }
-  };
+}
+
+Question.prototype.options = function(params) {
+  // body...
+  for (var i = 0; i < params.length; i++) {
+    console.log(i + ' - ' + params[i] ); 
+  }
+};
+
+// questions
+var q1  = new Question('Do you like JavaScript?', 'yes');
+var q2 = new Question('lerning js is ??', 'fun');
+var q3 = new Question('js is a .....', 'oop language');
+// answer arrays
+var options1 =['yes', 'no'];
+var options2 = ['hard to learn language', 'oop language', 'inheritance nonsupprter language'];
+var options3 = ['boring','altemate hard', 'fun', 'stupid'];
+// array of questions and options
+var questions = [q1, q2, q3];
+var answerOptions = [options1, options2, options3];
+// 
+function showQuestion(){
+  var random = randomNum();
+  console.log(questions[random].question);
+  questions[random].options(answerOptions[random]);
+  console.log('-----------------------------------------');
 }
 
 
-var q1  = new Question('Do you like JavaScript?', 'yes');
-q1.options(['yes', 'no']);
+// random number
+function randomNum(){
+  return Math.floor(Math.random() * 3 );
+}
+
+
+// calling functions
+console.log(randomNum());
+showQuestion();
