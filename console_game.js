7eabc5b3701748c4f714581b07b31f1b557c9586
wael ali <waelali595@gmail.com>
2018-02-06 00:38:27 +0100

    refined code and simple approche

--- console_game.js
+++ console_game.js
@@ -1,89 +1,51 @@
-// the function costructor
-var Question  = function(question, rightAnswer){
-  this.question = question;
-  this.rightAnswer  = rightAnswer;
-}
-// inheritance through the protptype property
-Question.prototype.showOptions = function(params) {
-  // body...
-  for (var i = 0; i < params.length; i++) {
-    console.log(i + ' - ' + params[i] ); 
-  }
-};
-
-// variabls
-var userAnswer, score , playing, counter;
-init();
-// questions
-var q1 = new Question('Do you like JavaScript?', 'yes');
-var q2 = new Question('lerning js is ??', 'fun');
-var q3 = new Question('js is a .....', 'oop language');
-// answers arrays
-var options1 = ['yes', 'no'];
-var options3 = ['hard to learn language', 'oop language', 'inheritance nonsupprter language'];
-var options2 = ['boring','altemate hard', 'fun', 'stupid'];
-// array of questions and options
-var questions = [q1, q2, q3];
-var answerOptions = [options1, options2, options3];
-
-function showQuestion(){
-  var random = randomNum();
-  console.log(questions[random].question);
-  questions[random].showOptions(answerOptions[random]);
+(function(){
   
-  getAnswer('answer the question in console!');
-  // doesn't continue till user enter a number
-  while (isNaN(userAnswer)){getAnswer('Enter a number only!!');}
-  if(answersArray(answerOptions[random]).includes(userAnswer)){
-    //console.log(answerOptions.indexOf(questions[random].rightAnswer));
-   if (userAnswer === answerOptions[random].indexOf(questions[random].rightAnswer)) {
-      console.log('brelient!!');
-      score += 1;
-      console.log('-----------------------------------------');
-    }
-    console.log('Your score: ' + score);
 
+function Question(question, answers, correct){
+  this.question = question;
+  this.answers  = answers;
+  this.correct  = correct;
+}
+
+Question.prototype.displayQuestion = 
+function(){
+  console.log(this.Question);
+
+  for (var i = 0; i < this.answers.length; i++) {
+    console.log(i + '-' + this.answers[i]);
+  }
+}
+
+Question.prototype.checkAnswer = 
+function(ans){
+  if (ans === this.correct){
+    console.log('Correct Answer')
   }else{
-    console.log('Ooops!!, doesn\'t belongs to the options');
-    console.log('-----------------------------------------');
+    console.log('Wrong Answer. Try Again!');
   }
 }
 
-function getAnswer(message){
-  userAnswer =  Math.floor(prompt(message));
-  //if (true) {}
-}
 
-// random number
-function randomNum(){
-  return Math.floor(Math.random() * 3 );
-}
+var q1 = new Question(
+  "Is Berlin biger than Hamburg?",
+  ["yes", "no"],
+  0
+  );
+var q2 = new Question(
+  "what is the name of france capital",
+  ["london", "paris", "newyork"],
+  1
+  );
+var q3 = new Question(
+  "visiting china is ...?",
+  ["boring", "dangerous", "Exiting", "bad"],
+  2
+  );
 
-// array of indexs of the answer options
-function answersArray(arr){
-  var anArr = [];
-  for (var i = 0; i < arr.length; i++) {
-    anArr.push(i);
-  }
-  return anArr;
-}
-// seting some value
-function init(){
-  userAnswer = 10;
-  score = 0;
-  playing = true;
-  counter = 0;
-}
-
-
-
-// calling functions
-while(playing){
-  showQuestion();
-  counter++;
-  if (counter === 10) {
-    playing = false;
-  }
-}
-
+var questions = [q1, q2, q3];
+var n = Math.floor(Math.random() * questions.length);
+questions[n].displayQuestion();
+var answer = parseInt(prompt("Please select the correct answer"));
+questions[n].checkAnswer(answer);
+})();
 
